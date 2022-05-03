53b47fd428b2c03ad428a6b986efb76744bf1003
Gregory-JP <63425849+Gregory-JP@users.noreply.github.com>
2022-05-03 17:31:59 -0300

    Add files via upload

---
+++ criando_objetos.R
@@ -0,0 +1,25 @@
+numeros <- c(1, 2, 3) # c() utilizando para concatenar
+numeros
+
+# R é case sensitive
+Numeros = 1:6; Numeros
+
+# soma Numeros
+sum(Numeros)
+
+# tamanho Numeros
+length(Numeros)
+
+# media Numeros
+sum(Numeros) / length(Numeros) # calculo manual
+
+mean(Numeros)
+
+# menor e maior valor, respectivamente
+min(Numeros); max(Numeros)
+
+# variância Numeros
+var(Numeros)
+
+# seq() função para construir uma sequência
+sequencia <- seq(from=1, to=3, by=0.5); sequencia
